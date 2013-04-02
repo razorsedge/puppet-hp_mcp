74dc61ef3f4b858e2686c4e7176bce2cbdde972f
Michael Arnold <github@razorsedge.org>
2013-04-01 23:10:14 -0700

    Add tests for main class.
    Updated tests/init.pp to be a parameterized class.
    Added rspec tests for Class['hp_mcp'].

--- tests/init.pp
+++ tests/init.pp
@@ -9,4 +9,6 @@
 # Learn more about module testing here:
 #  http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-include hp_mcp
+class { 'hp_mcp':
+  cmalocalhostrwcommstr => 'SomeSecureString',
+}
