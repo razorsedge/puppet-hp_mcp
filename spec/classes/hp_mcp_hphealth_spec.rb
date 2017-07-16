934bc1bc6d5a5cfbded532c97ee6b982a77416d2
Michael Arnold <github@razorsedge.org>
2017-07-16 13:59:21 -0400

    Support STRICT_VARIABLES for Puppet 4.

--- spec/classes/hp_mcp_hphealth_spec.rb
+++ spec/classes/hp_mcp_hphealth_spec.rb
@@ -7,7 +7,8 @@ describe 'hp_mcp::hphealth', :type => 'class' do
   context 'on a non-supported operatingsystem' do
     let :facts do {
       :osfamily        => 'foo',
-      :operatingsystem => 'foo'
+      :operatingsystem => 'foo',
+      :operatingsystemrelease => '1'
     }
     end
     it 'should fail' do
@@ -25,6 +26,7 @@ describe 'hp_mcp::hphealth', :type => 'class' do
         let(:params) {{}}
         let :facts do {
           :operatingsystem => os,
+          :operatingsystemrelease => '6.0',
           :manufacturer    => 'foo'
         }
         end
@@ -45,6 +47,7 @@ describe 'hp_mcp::hphealth', :type => 'class' do
       context "for operatingsystem #{os}" do
         let :facts do {
           :operatingsystem => os,
+          :operatingsystemrelease => '6.0',
           :manufacturer    => 'HP'
         }
         end
